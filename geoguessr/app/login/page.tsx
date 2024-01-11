f8399e78ed834d390388c39f9d41974fa4d9b232
remybms <remy.bamas@ynov.com>
2024-01-11 13:23:15 +0100

    edit page style

--- geoguessr/app/login/page.tsx
+++ geoguessr/app/login/page.tsx
@@ -29,20 +29,20 @@ export default function Login() {
             <div className="grid grid-cols-3 content-center ml-80 mr-80">
                 <div className="flex flex-col items-center justify-between bg-blue rounded-l-xl pt-16 pb-16 text-white">
                     <img src="/logo_ynov_campus_blanc.svg" width={150} />
-                    <div className="mt-8">Bienvenue dans la communauté Ynov</div>
+                    <div className="mt-8 font-bold text-xl">Bienvenue dans la communauté Ynov</div>
                     <div className="m-8">Candidats, Etudiants, Professeurs et Entreprises : accédez aux outils et services Ynov Campus à l'aide de votre Compte Ynov !</div>
-                    <div>
+                    <div className="border-2 border-white rounded-lg p-2 pl-8 pr-8">
                         Créer un compte
                     </div>
 
                 </div>
                 <form className="flex flex-col items-center justify-center space-y-4 bg-white rounded-r-xl col-span-2 pt-16 pb-16" onSubmit={log}>
                     <div className="text-blue text-4xl font-bold">Connexion</div>
-                    <div className="flex flex-col w-3/4">
+                    <div className="flex flex-col w-3/5">
                         <label htmlFor="mail">E-mail :</label>
                         <input type="email" id="mail" name="mail" required className="border-2 rounded-lg p-2" onChange={e => setMail(e.target.value)} />
                     </div>
-                    <div className="flex flex-col w-3/4">
+                    <div className="flex flex-col w-3/5">
                         <label htmlFor="password">Mot de passe :</label>
                         <input type="password" id="password" name="password" required className="border-2 rounded-lg p-2" onChange={e => setPassword(e.target.value)} />
                     </div>
